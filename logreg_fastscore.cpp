96e6b5cdd73c86807c935a0ac69b7e2d2475ef5c
merhi-odg <sami.merhi@opendatagroup.com>
2021-02-03 15:58:24 -0600

    test nlohman, add comments

--- logreg_fastscore.cpp
+++ logreg_fastscore.cpp
@@ -14,7 +14,7 @@
 #include <iostream>
 #include <fstream>
 
-//#include <nlohmann/json.hpp>
+#include <nlohmann/json.hpp>
 
 
 // modelop.init
@@ -66,8 +66,11 @@ void action(fastscore_value_t v, int slot, int seqno) {
     printf("The input array has %d elements\n", count);
 
     for (i = 0; i < count; i++)
-    {
+    {   
+        // get record (JSON) from array of records
         json_t *record = json_array_get(a,i);
+
+        // extract key:values pairs and store values as double
         json_t *duration_months = json_object_get(record, "duration_months");
         json_t *credit_amount = json_object_get(record, "credit_amount");
         json_t *installment_rate = json_object_get(record, "installment_rate");
@@ -84,13 +87,16 @@ void action(fastscore_value_t v, int slot, int seqno) {
         double number_existing_credits_value= json_number_value(number_existing_credits);
         double number_people_liable_value= json_number_value(number_people_liable);
 
+        // Form a vector of input record values
         std::vector<float> input_record = {
             duration_months_value, credit_amount_value, installment_rate_value,
             present_residence_since_value, age_years_value, number_existing_credits_value,
             number_people_liable_value};
 
+        // compute the probablity of class 2
         float pred_probability = expit(dotProduct(coefficients, input_record) + intercept);
 
+        // map probability to to classes {1,2}
         int prediction;
 
         if (pred_probability <= 0.5) {
@@ -99,14 +105,19 @@ void action(fastscore_value_t v, int slot, int seqno) {
             prediction = 2;
         }
 
+        // declare json object of prediction
 	    json_t *response=json_object();
 
 	    json_object_set(response, "prediction", json_real(prediction));
-	    json_array_append_new(b, response );
+	    
+        // append prediction to overall output
+        json_array_append_new(b, response );
 	    printf( "Just appended to array, current length: %d\n", v, json_array_size(b));
     }
     
     printf("Before fastscore_emit\n");
+
+    // return predictions
     fastscore_emit((fastscore_value_t) {
         .fmt = FASTSCORE_FMT_JSON,
         .js = b,
