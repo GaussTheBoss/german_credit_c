f655bd5dc11a8a2e79344958e422c6e8622b7126
merhi-odg <sami.merhi@opendatagroup.com>
2021-02-03 18:31:07 -0600

    Update logreg_fastscore.cpp

--- logreg_fastscore.cpp
+++ logreg_fastscore.cpp
@@ -35,7 +35,8 @@ void begin()
     double *coefficients_values = (double *) malloc(json_array_size(coefficients) * sizeof(double));
 
     for(i = 0; i < json_array_size(coefficients); i++)
-        coefficients_values[i] = json_number_value(json_array_get(coefficients, i));
+        json_t *tmp = json_array_get(coefficients, i)
+        coefficients_values[i] = json_number_value(tmp);
 
     //json_t *coefficients = json_object_get(weights, "coefficients");
     //std::vector<float> coefficients_value = json_array_get(coefficients, 0);
